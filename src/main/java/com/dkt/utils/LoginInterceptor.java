e0babb4f37b87e1774143e3814cac482e4fbe64c
doukangtai <2636693728@qq.com>
2020-11-28 23:20:50 +0800

    update

--- src/main/java/com/dkt/utils/LoginInterceptor.java
+++ src/main/java/com/dkt/utils/LoginInterceptor.java
@@ -19,15 +19,21 @@ public class LoginInterceptor implements HandlerInterceptor {
         response.setContentType("text/html;charset=utf-8");
         if (requestURI.contains("admin")) {
             Object admin = session.getAttribute("admin");
-            response.getWriter().write("登录管理员账号后才能访问");
+            if (admin == null) {
+                response.getWriter().write("登录管理员账号后才能访问");
+            }
             return admin != null;
         } else if (requestURI.contains("employee")) {
             Object employee = session.getAttribute("employee");
-            response.getWriter().write("登录员工账号后才能访问");
+            if (employee == null) {
+                response.getWriter().write("登录员工账号后才能访问");
+            }
             return employee != null;
         } else if (requestURI.contains("customer")) {
             Object customer = session.getAttribute("customer");
-            response.getWriter().write("登录顾客账号后才能访问");
+            if (customer == null) {
+                response.getWriter().write("登录顾客账号后才能访问");
+            }
             return customer != null;
         }
         return true;
